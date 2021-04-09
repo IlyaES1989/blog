e72ee800e9a69cb6491c39681f3dc4dc2a618e8f
ilyaes1989 <ilyaes1989@mail.ru>
2021-04-10 02:01:21 +0500

    Dockerize the application

--- blog/settings.py
+++ blog/settings.py
@@ -28,12 +28,12 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, './templates')
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_key('secret.key')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
 
 
 # Application definition
@@ -131,9 +131,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = 'static/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = get_key('mail_host.key')
-EMAIL_PORT = 587
-EMAIL_HOST_USER = get_key('mail_host_user.key')
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+# EMAIL_HOST = get_key('mail_host.key')
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = get_key('mail_host_user.key')
+# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = get_key('mail_pass.key')
+# EMAIL_HOST_PASSWORD = get_key('mail_pass.key')
