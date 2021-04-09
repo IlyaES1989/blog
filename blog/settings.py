367dc7eec8ce13289dac96086a2a825bf68777c2
ilyaes1989 <ilyaes1989@mail.ru>
2021-04-10 02:39:11 +0500

    The bug in docker-compose has rectified.

--- blog/settings.py
+++ blog/settings.py
@@ -13,13 +13,6 @@ import os
 from pathlib import Path
 
 
-def get_key(name):
-    secret_path = os.path.join(BASE_DIR, 'keys', '%s' % name)
-    with open(secret_path) as file:
-        key = file.read()
-    return key
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, './templates')
